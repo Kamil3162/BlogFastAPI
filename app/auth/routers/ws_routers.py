4daf9ff1c9edab91215b369c5b2f0c5f4febf22f
Kamil Holub <kamilholub_1@wp.pl>
2024-02-28 21:24:41 +0100

    Fix WebSockets

--- app/auth/routers/ws_routers.py
+++ app/auth/routers/ws_routers.py
@@ -9,23 +9,42 @@ create_ws_app = APIRouter()
 async def websocket_endpoint(websocket: WebSocket, user_id: str):
 
     print(websocket, user_id)
+    if user_id:
+        await manager.connect(user_id, websocket)
+        await manager.send_online_status(user_id)
 
-    manager.connect(user_id, websocket)
+        now = datetime.datetime.now()
+        current_time = now.strftime("%H:%M")
 
-    now = datetime.datetime.now()
-    current_time = now.strftime("%H:%M")
+        # filter and send message to particular user based on id
 
-    try:
-        while True:
-            data = await websocket.receive_text()
-            # await manager.send_personal_message(f"You wrote: {data}", websocket)
+        try:
+            while True:
+                data_str = await websocket.receive_text()   # text get from ws/react
+
+                try:
+                    data = json.loads(data_str)
+                    print(data)
+
+                    target_user_id = data.get("target_usr")
+                    source_user_id = data.get("source_usr")
+
+                    if target_user_id:
+                        ws_socket = manager.get_ws_by_userid(target_user_id)
+                        # await manager.send_personal_message(
+                        #     json.dumps(message), target_user_id)
+
+                except json.JSONDecodeError:
+                    print("This data isnt in json data")
+
+                message = {"time": current_time, "clientId": user_id,
+                           "message": "hello back response from server"}
+                await manager.broadcast_message(message)
+
+        except WebSocketDisconnect:
             message = {"time": current_time, "clientId": user_id,
-                       "message": data}
-            await manager.broadcast(json.dumps(message))
+                       "message": "Offline"}
+            await manager.broadcast_message(json.dumps(message))
+            await manager.disconnect(user_id)
 
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        message = {"time": current_time, "clientId": user_id,
-                   "message": "Offline"}
-        await manager.broadcast(json.dumps(message))
 
--- app/services/WsManager.py
+++ app/services/WsManager.py
@@ -1,29 +1,51 @@
+import json
+
 from fastapi import WebSocket, FastAPI, APIRouter
-from typing import List
+from typing import List, Dict
 
 class WsManager:
     def __init__(self):
-        self.active_connections: List[str, WebSocket] = dict()
+        self.active_connections: Dict[str, WebSocket] = dict()
 
     async def connect(self, user_id: str, websocket: WebSocket):
-        print("connect to ws")
+        print("Connecting to ws", user_id)
         await websocket.accept()
-        self.active_connections.append(websocket)
+        self.active_connections[user_id] = websocket
 
-    def disconnect(self, user_id: str, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+    async def disconnect(self, user_id: str):
+        connection = self.active_connections.pop(user_id, None)
+        if connection:
+            await connection.close()
+            print("Disconnected", user_id)
 
     async def send_personal_message(self, message: str, user_id: str):
-        websocket = self.active_connections[user_id]
-        websocket.send_text(message)
+        websocket = self.active_connections.get(user_id)
+        if websocket:
+            try:
+                await websocket.send_text(message)
+            except RuntimeError as e:
+                print(f"Error sending message to {user_id}: {e}")
 
     async def broadcast_message(self, message):
-        for connection in self.active_connections:
-            connection.send(message)
-
-    async def send_online_status(self):
+        print("to jest broadcast message")
         for user_id, connection in self.active_connections.items():
-            connection.send(str(user_id))
+            try:
+                await connection.send_text(json.dumps(message))
+            except RuntimeError:
+                # Mark user for disconnection if message sending fails
+                print("error broadcast message")
+
+    async def send_online_status(self, user_id: str):
+        data = {
+            'type': 'info_status',
+            'status': 'online',
+            'user_id': user_id,
+            'message': "32"
+        }
+        await self.broadcast_message(json.dumps(data))
+    async def get_ws_by_userid(self, user_id: str):
+        return self.active_connections.get(user_id)
+
 
 manager = WsManager()
 
